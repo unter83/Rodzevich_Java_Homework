136a110abb22f3f1654d3ffb87c943a2acaf0c2d
Unter <unter83@gmail.com>
2023-03-04 23:07:14 +0300

    Made task 3-3

--- Homework_3/task3_3.java
+++ Homework_3/task3_3.java
@@ -5,14 +5,18 @@ import java.util.ArrayList;
 
 public class task3_3 {
     public static void main(String[] args) {
+        System.out.println("\nПрограмма выполняет сортировку методом слияния\n");
+        System.out.println("Исходный список:");
         int[] unsorted_array = new int[] {2, 4, 12, 68, 29, 6, 48, 62, 7, 8, 10};
         ArrayList<Integer> unsorted_list = new ArrayList();
         for (int index = 0; index < unsorted_array.length; index++) {
             unsorted_list.add(unsorted_array[index]);
         }
         System.out.println(unsorted_list);
-        unsorted_list = SortList(unsorted_list);
-        System.out.println(unsorted_list);
+        ArrayList<Integer> sorted_list = new ArrayList();
+        sorted_list = SortList(unsorted_list);
+        System.out.println("Список после сортировки");
+        System.out.println(sorted_list);
     }    
 
     public static ArrayList SortList(ArrayList<Integer> list) {
@@ -27,13 +31,13 @@ public class task3_3 {
         for (int i = 0; i < list.size()/2; i++) {
             list_A.add(list.get(i));
         }
-        System.out.println(list_A);
+        // System.out.println(list_A);
 
         ArrayList <Integer> list_B = new ArrayList(list.size());
         for (int i = list.size()/2; i < list.size(); i++) {
             list_B.add(list.get(i));
         }
-        System.out.println(list_B);
+        // System.out.println(list_B);
 
         list_A = SortList(list_A);
         list_B = SortList(list_B);
