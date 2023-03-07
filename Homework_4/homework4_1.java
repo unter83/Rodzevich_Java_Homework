8e955f26ac31f246d9044a0edfd947a17a4fd53b
Unter <unter83@gmail.com>
2023-03-07 21:32:51 +0300

    finish task 4-1

---
+++ Homework_4/homework4_1.java
@@ -0,0 +1,30 @@
+package Homework_4;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class homework4_1 {
+    public static void main(String[] args) {
+        System.out.println("\nПрограмма реализует не void метод возвращающий перевернутый список организованный Linkedlist'ом\n");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите последовательность чисел разделяя из пробелом: ");
+        String msg = sc.nextLine();
+        String[] arr_str = msg.split(" ");
+        LinkedList ll = new LinkedList<>(Arrays.asList(arr_str));
+        ll =  ReverseLL(ll);
+        for (Object item : ll) {
+            System.out.print(item + " ");
+        }
+        
+    }
+
+    public static LinkedList ReverseLL(LinkedList ll) {
+        LinkedList rll = new LinkedList<>();
+        for(Object item : ll) {
+            rll.addFirst(item);
+        }
+        return rll;
+    }
+
+}
