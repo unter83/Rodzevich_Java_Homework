340bcaf7c8b969713efa1245f1f2192ebe4e0e52
Unter <unter83@gmail.com>
2023-03-08 13:29:03 +0300

    finish task4-2

--- Homework_4/task4_1.java
+++ Homework_4/task4_1.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class homework4_1 {
+public class task4_1 {
     public static void main(String[] args) {
         System.out.println("\nПрограмма реализует не void метод возвращающий перевернутый список организованный Linkedlist'ом\n");
         Scanner sc = new Scanner(System.in);
-        System.out.print("Введите последовательность чисел разделяя из пробелом: ");
+        System.out.print("Введите последовательность разделяя из пробелом: ");
         String msg = sc.nextLine();
         String[] arr_str = msg.split(" ");
         LinkedList ll = new LinkedList<>(Arrays.asList(arr_str));
---
+++ Homework_4/task4_2.java
@@ -0,0 +1,73 @@
+package Homework_4;
+
+import java.lang.ProcessBuilder.Redirect.Type;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class task4_2 {
+    public static void main(String[] args) {
+        System.out.println("\nПрограмма реализует очередь с помощью Linkedlist и реализует 3 команды:");
+        System.out.println("1. помещает элементы в конец очереди");
+        System.out.println("2. denqueue - возвращает первый элемент из очереди и удаляет его");
+        System.out.println("3. first - возвращает первый элемент из очереди, не удаляя");
+        System.out.println("4. print - возвращает все элементы всей очереди");
+        System.out.println("4. exit - выход");
+
+        String command = "";
+        Queue ll = new LinkedList<>();
+        boolean exit = false; 
+        while (exit == false) {
+            System.out.print("Введите элементы очереди или команду: ");
+            Scanner sc = new Scanner(System.in, "cp866");
+            String msg = sc.nextLine();
+            String[] msg_arr = msg.split(" ");
+            command = msg_arr[0];
+            switch (command) {
+                case "first": {
+                    System.out.println(ll.peek());
+                    break;
+                }
+                case "denqueue": {
+                    System.out.println(ll.remove());                
+                    break;
+                }
+                case "print": {
+                    PrintQueue(ll);               
+                    break;
+                }
+                case "exit":
+                {
+                    exit = true;
+                    break; 
+                }
+                default: {
+                    ll =  AddToQueue(msg_arr, ll);            
+                    break;                    
+                }
+            }
+        }   
+
+    }
+
+    public static Queue AddToQueue(String[] array, Queue ll) {   
+        for (int i = 0; i < array.length; i++) {
+            ll.add(array[i]);
+        }
+        return ll;            
+    }
+
+    public static void PrintQueue(Queue ll) {
+        for (Object item : ll) {
+            System.out.print(item + " ");        
+        }
+        System.out.println();
+
+    }
+
+}
